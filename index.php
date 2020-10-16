bdeb92ede22777a034ed99195e6c55f68d7bfe18
ducviethaboo <70750327+ducviethaboo@users.noreply.github.com>
2020-10-16 17:02:46 +0700

    Commit 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'Stack.php';
+
+$stack = new Stack(5);
+$stack->push(1);
+$stack->push(1);
+$stack->push(1);
+$stack->push(1);
+$stack->push(1);
+$stack->push(1);
+
+//$stack->top();
